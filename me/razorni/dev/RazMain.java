56fea1b0ba274fcc81c9e6586f849c18e8d22cd4
Nenad Bojanić <bojanicsone@gmail.com>
2020-12-11 22:22:51 +0100

    Create RazMain.java

---
+++ me/razorni/dev/RazMain.java
@@ -0,0 +1,36 @@
+package me.razorni.dev;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import me.razorni.dev.listeners.RefillSignListener;
+import me.razorni.dev.listeners.TrashSignListener;
+
+public class RazMain extends JavaPlugin {
+	
+    public static RazMain instance;
+    
+    public void onEnable() {
+		Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "[RazSigns] Plugin has been loaded successfully.");
+        instance = this;    
+        this.registerListeners();
+
+    }
+    
+    private void registerListeners() {
+        PluginManager manager = this.getServer().getPluginManager();
+	    manager.registerEvents(new RefillSignListener(this), this);
+	    manager.registerEvents(new TrashSignListener(this), this);
+	    
+    }
+    
+    public void onDisable() {
+    	
+    }
+
+    public static RazMain getInstance() {
+        return instance;
+        
+      }
+}
